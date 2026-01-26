f63c69a578f8765eb59b45a3a5f348d83cedc532
Maxwell939 <maksimka.frolov.10@outlook.com>
2026-01-26 23:24:05 +0300

    feat: added subclass of Enemy: EnemyBird

--- enemies.py
+++ enemies.py
@@ -1,4 +1,8 @@
+import random
+
 import arcade
+
+from constants import RIGHT_FACING, LEFT_FACING, SCREEN_WIDTH, ENEMY_BIRD_SPEED
 
 
 class Enemy(arcade.Sprite):
@@ -9,3 +13,15 @@ class Enemy(arcade.Sprite):
         super().update(delta_time)
         if self.collides_with_sprite(player):
             self.kill()
+
+
+class EnemyBird(Enemy):
+    def __init__(self):
+        super().__init__()
+        for i in range(1, 10):
+            self.textures.append(arcade.load_texture(f"textures/bird/bird{i}"))
+
+        self.cur_texture_index = 0
+
+    def update(self, player, delta_time: float = 1 / 60) -> None:
+        super().update(delta_time)
