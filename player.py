78172617b98e7697310b0a86ae0792a29732a889
Maxwell939 <maksimka.frolov.10@outlook.com>
2026-01-28 21:37:57 +0300

    fix: returned the new player's textures
    feat: added new texture for player

--- player.py
+++ player.py
@@ -1,18 +1,19 @@
 import arcade
 
-from constants import SCREEN_WIDTH, SCROLL_THRESHOLD
+from constants import SCREEN_WIDTH, SCROLL_THRESHOLD, PLAYER_SCALE
 
 
 class Player(arcade.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.textures.append(arcade.load_texture("textures/player/fall_right.png"))
-        self.textures.append(arcade.load_texture("textures/player/idle_right.png"))
-        self.textures.append(arcade.load_texture("textures/player/jump_right.png"))
+        self.textures.append(arcade.load_texture("textures/player/fall.png"))
+        self.textures.append(arcade.load_texture("textures/player/idle.png"))
+        self.textures.append(arcade.load_texture("textures/player/jump.png"))
         self.texture = self.textures[1]
 
         self.center_x = x
         self.bottom = y
+        self.scale = PLAYER_SCALE
 
         self.scroll = 0
 
@@ -24,9 +25,16 @@ class Player(arcade.Sprite):
             self.right = 0
 
         if self.change_x > 0:
-            self.scale_x = 1
+            self.scale_x = PLAYER_SCALE
         elif self.change_x < 0:
-            self.scale_x = -1
+            self.scale_x = -PLAYER_SCALE
+
+        if self.change_y > 0:
+            self.texture = self.textures[2]
+        elif self.change_y < 0:
+            self.texture = self.textures[0]
+        else:
+            self.texture = self.textures[1]
 
         self.scroll = 0
         if self.top >= SCROLL_THRESHOLD:
