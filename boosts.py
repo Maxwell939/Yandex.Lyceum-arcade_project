e48dc32860f23939b14444af4023e23ddb3cf5a8
Lev Pro <leo.prozarov@mail.ru>
2026-03-17 22:36:09 +0300

    fix: textures boosts were changed

--- boosts.py
+++ boosts.py
@@ -3,8 +3,6 @@ import sys
 import os
 from constants import JUMP_SPEED
 from player import Player
-
-
 def get_base_path():
     if getattr(sys, 'frozen', False):
         return sys._MEIPASS
@@ -18,13 +16,14 @@ class Boost(arcade.Sprite):
     def update(self, player: arcade.Sprite, delta_time: float = 1 / 60):
         super().update(delta_time)
         if self.collides_with_sprite(player) and player.change_y < 0:
-            player.change_y = JUMP_SPEED * 2
+            player.change_y = JUMP_SPEED * self.boost_strength
             if player.change_y > 0:
                 player.boost_active = True
 class Spring(Boost):
     def __init__(self):
         super().__init__()
-        string_path = os.path.join(BASE_PATH, "textures", "boosts", "string.png")
-        self.texture = arcade.load_texture(string_path)
-        self.scale_y = 0.02
-        self.scale_x = 0.07
+        string_path = os.path.join(BASE_PATH, "textures", "boosts", "spring", "spring1.png")
+        self.textures = []
+        self.textures.append(arcade.load_texture(string_path))
+        self.scale = 1.5
+        self.boost_strength = 2
